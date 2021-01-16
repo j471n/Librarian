a39f25f3c27a752b65d9c0336df5dba9db764252
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-01-16 20:06:45 +0530

    Adding Image to Background and Setting up Icons of Buttons

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from addBook import *
 from viewsBook import *
 from deletebook import *
 from issueBook import *
+from showDB import *
 from returnBook import *
 from os import getenv
 from dotenv import load_dotenv
@@ -21,20 +22,9 @@ root.minsize(width=400, height=400)
 root.geometry("600x500")
 
 
-# 1.4
-
-# same = True
-# n = 0.25
-
 # Adding a background image
-background_image = Image.open("img/background/librarian.jpg")
-[imageSizeWidth, imageSizeHeight] = background_image.size
-img = ImageTk.PhotoImage(background_image)
-
-Canvas1 = Canvas(root)
-Canvas1.create_image(100, 250, image=img)
-Canvas1.config(bg="white", width=imageSizeWidth, height=imageSizeHeight)
-Canvas1.pack(expand=True, fill=BOTH)
+img = ImageTk.PhotoImage(Image.open("img/background/librarian.jpg"))
+Label(root, image=img).pack()
 
 
 
@@ -58,19 +48,19 @@ img5 = PhotoImage(file="img/buttons/return-32.png")
 
 # Resizing image to fit on button
 
-btn1 = Button(root,text="Add Book", bg='black', fg='white', image=img1, compound=LEFT, padx=25, font=('Gill Sans MT', 12), command=addBook)
+btn1 = Button(root,text="Add Book", bg='black', fg='white', image=img1, compound=LEFT, padx=40, font=('Gill Sans MT', 12), anchor=CENTER, command=addBook)
 btn1.place(relx=0.28,rely=0.35, relwidth=0.45,relheight=0.1)
 
-btn2 = Button(root,text="Delete Book",bg='black', fg='white', image=img2, compound=LEFT, padx=25, font=('Gill Sans MT', 12), command=delete)
+btn2 = Button(root,text="Delete Book",bg='black', fg='white', image=img2, compound=LEFT, padx=25, font=('Gill Sans MT', 12),anchor=CENTER, command=delete)
 btn2.place(relx=0.28,rely=0.45, relwidth=0.45,relheight=0.1)
 
-btn3 = Button(root,text="View Details",bg='black', fg='white', image=img3, compound=LEFT, padx=25, font=('Gill Sans MT', 12), command=View)
+btn3 = Button(root,text="View Details",bg='black', fg='white', image=img3, compound=LEFT, padx=25, font=('Gill Sans MT', 12), anchor=CENTER,command=Show)
 btn3.place(relx=0.28,rely=0.55, relwidth=0.45,relheight=0.1)
 
-btn4 = Button(root,text="Issue Book",bg='black', fg='white', image=img4, compound=LEFT, padx=25, font=('Gill Sans MT', 12), command=issueBook)
+btn4 = Button(root,text="Issue Book",bg='black', fg='white', image=img4, compound=LEFT, padx=40, font=('Gill Sans MT', 12), anchor=CENTER,command=issueBook)
 btn4.place(relx=0.28,rely=0.65, relwidth=0.45,relheight=0.1)
 
-btn5 = Button(root,text="Return Book",bg='black', fg='white', image=img5, compound=LEFT, padx=25, font=('Gill Sans MT', 12), command=returnBook)
+btn5 = Button(root,text="Return Book",bg='black', fg='white', image=img5, compound=LEFT, padx=25, font=('Gill Sans MT', 12),anchor=CENTER, command=returnBook)
 btn5.place(relx=0.28,rely=0.75, relwidth=0.45,relheight=0.1)
 root.resizable(0,0)
 root.mainloop()
