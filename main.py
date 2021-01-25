323f49f6de35c80cfe9d2b9b11e7a6ae6b1d615b
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-01-25 16:23:29 +0530

    Adding Images to button to look more attractive

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from returnBook import *
 from os import getenv
 from dotenv import load_dotenv
 from feedback import feedBack
+from about import aboutUS
 
 load_dotenv()
 # Function form viewsBook.py to connect DB
@@ -40,32 +41,36 @@ headingLabel.place(relx=0, rely=0, relwidth=1, relheight=1)
 
 
 # Creating a photoimage object to use image
-img1 = PhotoImage(file="img/buttons/add2-32.png")
-img2 = PhotoImage(file="img/buttons/del-32.png")
-img3 = PhotoImage(file="img/buttons/info-32.png")
-img4 = PhotoImage(file="img/buttons/issue2-32.png")
-img5 = PhotoImage(file="img/buttons/return-32.png")
+img1 = PhotoImage(file="img/buttons/add.png")
+img2 = PhotoImage(file="img/buttons/del.png")
+img3 = PhotoImage(file="img/buttons/view.png")
+img4 = PhotoImage(file="img/buttons/issue.png")
+img5 = PhotoImage(file="img/buttons/return.png")
 img6 = PhotoImage(file="img/buttons/f-icon.png")
+img7 = PhotoImage(file="img/buttons/info-icon.png")
 
 # Resizing image to fit on button
 
-btn1 = Button(root,text="Add Book", bg='black', fg='white', image=img1, compound=LEFT, padx=40, font=('Gill Sans MT', 12), anchor=CENTER, command=addBook)
+btn1 = Button(root,text="", bg='black', fg='white', image=img1, border=0.5, compound=LEFT, padx=40, font=('Gill Sans MT', 12), anchor=CENTER, command=addBook)
 btn1.place(relx=0.28,rely=0.35, relwidth=0.45,relheight=0.1)
 
-btn2 = Button(root,text="Delete Book",bg='black', fg='white', image=img2, compound=LEFT, padx=25, font=('Gill Sans MT', 12),anchor=CENTER, command=delete)
+btn2 = Button(root,text="",bg='black', fg='white', image=img2, border=0.5,compound=LEFT, padx=25, font=('Gill Sans MT', 12),anchor=CENTER, command=delete)
 btn2.place(relx=0.28,rely=0.45, relwidth=0.45,relheight=0.1)
 
-btn3 = Button(root,text="View Details",bg='black', fg='white', image=img3, compound=LEFT, padx=25, font=('Gill Sans MT', 12), anchor=CENTER,command=View)
+btn3 = Button(root,text="",bg='black', fg='white', image=img3, border=0.5,compound=LEFT, padx=25, font=('Gill Sans MT', 12), anchor=CENTER,command=View)
 btn3.place(relx=0.28,rely=0.55, relwidth=0.45,relheight=0.1)
 
-btn4 = Button(root,text="Issue Book",bg='black', fg='white', image=img4, compound=LEFT, padx=40, font=('Gill Sans MT', 12), anchor=CENTER,command=issueBook)
+btn4 = Button(root,text="",bg='black', fg='white', image=img4, border=0.5,compound=LEFT, padx=40, font=('Gill Sans MT', 12), anchor=CENTER,command=issueBook)
 btn4.place(relx=0.28,rely=0.65, relwidth=0.45,relheight=0.1)
 
-btn5 = Button(root,text="Return Book",bg='black', fg='white', image=img5, compound=LEFT, padx=25, font=('Gill Sans MT', 12),anchor=CENTER, command=returnBook)
+btn5 = Button(root,text="",bg='black', fg='white', image=img5, border=0.5,compound=LEFT, padx=25, font=('Gill Sans MT', 12),anchor=CENTER, command=returnBook)
 btn5.place(relx=0.28,rely=0.75, relwidth=0.45,relheight=0.1)
 
-btn6 = Button(root, text="", bg='black', fg='white', image=img6, compound=LEFT, padx=25, font=('Gill Sans MT', 12), anchor=CENTER, command=feedBack)
+btn6 = Button(root, text="", bg='black', border = 0.8, image=img6, compound=LEFT, padx=25, font=('Gill Sans MT', 12), anchor=CENTER, command=feedBack)
 btn6.place(relx=0.03, rely=0.87, relwidth=0.1, relheight=0.1)
+
+btn7 = Button(root, text="", bg='black', border = 0.8, image=img7, compound=LEFT, padx=25, font=('Gill Sans MT', 12), anchor=CENTER, command=aboutUS)
+btn7.place(relx=0.03, rely=0.77, relwidth=0.1, relheight=0.1)
 
 root.resizable(0,0)
 root.mainloop()
