a9389fb155c7a33c7b7b3eb3ee9330401c3ffdf4
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-01-29 20:38:41 +0530

    Updating SQL Query

--- mysql.sql
+++ mysql.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE db;
 
 -- To Creating books Table
-CREATE TABLE books (book_id VARCHAR(200) PRIMARY KEY, title VARCHAR(50), author VARCHAR(30),publication VARCHAR(100), status VARCHAR(30));
+CREATE TABLE books (book_id VARCHAR(200) PRIMARY KEY, title VARCHAR(50), author VARCHAR(30),publication VARCHAR(100), status VARCHAR(30), phyLocation VARCHAR(50), issued_date VARCHAR(15), issued_to VARCHAR(25));
 
--- To Creating Book_issued Table
-CREATE TABLE books_issued (bid VARCHAR(200) PRIMARY KEY, issuedto VARCHAR(50),issued_date VARCHAR(15));
+-- To Track the Book Position
+CREATE TABLE position (bid, VARCHAR(200) PRIMARY KEY, location VARCHAR(50));
