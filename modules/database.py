9c7b77d2bbf13f38bcefaec99ec47b2b9aad2928
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-02-14 05:15:58 +0530

    adding new table for the students

--- modules/database.py
+++ modules/database.py
@@ -20,11 +20,15 @@ def connectDB():
 
         # SQL
         q1 = "CREATE TABLE books (book_id varchar(200) NOT NULL, title varchar(50), author varchar(30),publication varchar(100), status varchar(30),phyLocation varchar(50),issued_date varchar(15),issued_to varchar(25),PRIMARY KEY (book_id));"
+        
         q2 = 'CREATE TABLE position (bid varchar(200) NOT NULL,location varchar(50),PRIMARY KEY (bid));'
+
+        q3 = "CREATE TABLE students (student_id INTEGER NOT NULL UNIQUE, student_name VARCHAR(50), dob VARCHAR(15), course VARCHAR(20), branch VARCHAR(10), address VARCHAR(100), contact VARCHAR(20), img BLOB, g_year INTEGER, PRIMARY KEY('student_id' AUTOINCREMENT));"
 
         con = sqlite3.connect(getenv('DATABASE'))
         con.execute(q1)
         con.execute(q2)
+        con.execute(q3)
         con.commit()
 
         print("Database Connected, New Created")
